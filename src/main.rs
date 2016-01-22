fc6aeb067f5043194684f3bfa174888770b86365
Evan Weiler <plasmids@mykolab.com>
2016-01-21 18:43:19 -0600

    Read in command line arguements

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+use std::env;
+
+#[derive(PartialEq)]
+enum Verbosity {
+    Quiet,
+    Verbose,
+    Extra,
+}
+
 fn main() {
-    
+    let mut args = env::args();
+    args.next();
+    let mut filename = "".to_string();
+    let mut verbosity = Verbosity::Quiet;
+    for arg in args {
+        match arg.as_str() {
+            "-v" => verbosity = Verbosity::Verbose,
+            "-V" => verbosity = Verbosity::Extra,
+            _ => filename = arg,
+        }
+    }
 }
