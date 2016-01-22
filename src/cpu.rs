481136c046474e5071a4d5d794310f6a9c385470
Evan Weiler <plasmids@mykolab.com>
2016-01-21 21:07:01 -0600

    Add one to max u16 value to get actual size

--- src/cpu.rs
+++ src/cpu.rs
@@ -1,7 +1,7 @@
 use std::u16;
 
-const RAM_SIZE: usize = u16::MAX as usize;
-const SP_HARD_UPPER: u16 = 0b00000001_00000000;
+const RAM_SIZE: usize = (u16::MAX as usize) + 1; //pc (program counter) is 16 bits
+const SP_HARD_UPPER: u16 = 0b00000001_00000000; // upper 8 bits of the 16 bit sp are hard coded to 00000001
 
 struct Cpu {
     mem: Box<[u8; RAM_SIZE]>,
